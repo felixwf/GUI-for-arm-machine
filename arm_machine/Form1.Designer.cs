1713b4494830223ab8d4965b209a45f0e1f3bec1
felix <felix@zuru.tech>
2019-07-15 09:04:23 +0800

    finish the basic version

--- arm_machine/Form1.Designer.cs
+++ arm_machine/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.baudrate = new System.Windows.Forms.TextBox();
@@ -38,8 +39,14 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton_wl = new System.Windows.Forms.RadioButton();
+            this.radioButton_wc = new System.Windows.Forms.RadioButton();
             this.button4 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.deactivateSucker = new System.Windows.Forms.Button();
+            this.activateSucker = new System.Windows.Forms.Button();
+            this.liftup = new System.Windows.Forms.Button();
+            this.dropdown = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.button3 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
@@ -53,15 +60,13 @@
             this.targetX = new System.Windows.Forms.TextBox();
             this.targetY = new System.Windows.Forms.TextBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.dropdown = new System.Windows.Forms.Button();
-            this.liftup = new System.Windows.Forms.Button();
-            this.activateSucker = new System.Windows.Forms.Button();
-            this.deactivateSucker = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -85,7 +90,7 @@
             // 
             this.progressBar1.Location = new System.Drawing.Point(34, 394);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(730, 16);
+            this.progressBar1.Size = new System.Drawing.Size(898, 17);
             this.progressBar1.TabIndex = 2;
             // 
             // serialport
@@ -123,11 +128,13 @@
             this.textBox1.Location = new System.Drawing.Point(6, 24);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(224, 334);
+            this.textBox1.Size = new System.Drawing.Size(401, 105);
             this.textBox1.TabIndex = 6;
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.radioButton_wl);
+            this.groupBox1.Controls.Add(this.radioButton_wc);
             this.groupBox1.Controls.Add(this.button4);
             this.groupBox1.Controls.Add(this.baudrate);
             this.groupBox1.Controls.Add(this.button1);
@@ -140,6 +147,28 @@
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Serialport";
+            // 
+            // radioButton_wl
+            // 
+            this.radioButton_wl.AutoSize = true;
+            this.radioButton_wl.Checked = true;
+            this.radioButton_wl.Location = new System.Drawing.Point(25, 260);
+            this.radioButton_wl.Name = "radioButton_wl";
+            this.radioButton_wl.Size = new System.Drawing.Size(92, 19);
+            this.radioButton_wl.TabIndex = 8;
+            this.radioButton_wl.TabStop = true;
+            this.radioButton_wl.Text = "wireless";
+            this.radioButton_wl.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_wc
+            // 
+            this.radioButton_wc.AutoSize = true;
+            this.radioButton_wc.Location = new System.Drawing.Point(25, 234);
+            this.radioButton_wc.Name = "radioButton_wc";
+            this.radioButton_wc.Size = new System.Drawing.Size(108, 19);
+            this.radioButton_wc.TabIndex = 7;
+            this.radioButton_wc.Text = "with cable";
+            this.radioButton_wc.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
@@ -166,6 +195,46 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Control";
             // 
+            // deactivateSucker
+            // 
+            this.deactivateSucker.Location = new System.Drawing.Point(135, 314);
+            this.deactivateSucker.Name = "deactivateSucker";
+            this.deactivateSucker.Size = new System.Drawing.Size(115, 23);
+            this.deactivateSucker.TabIndex = 9;
+            this.deactivateSucker.Text = "Deactivate";
+            this.deactivateSucker.UseVisualStyleBackColor = true;
+            this.deactivateSucker.Click += new System.EventHandler(this.deactivateSucker_Click);
+            // 
+            // activateSucker
+            // 
+            this.activateSucker.Location = new System.Drawing.Point(135, 285);
+            this.activateSucker.Name = "activateSucker";
+            this.activateSucker.Size = new System.Drawing.Size(115, 23);
+            this.activateSucker.TabIndex = 8;
+            this.activateSucker.Text = "Activate";
+            this.activateSucker.UseVisualStyleBackColor = true;
+            this.activateSucker.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // liftup
+            // 
+            this.liftup.Location = new System.Drawing.Point(7, 314);
+            this.liftup.Name = "liftup";
+            this.liftup.Size = new System.Drawing.Size(115, 23);
+            this.liftup.TabIndex = 7;
+            this.liftup.Text = "liftup";
+            this.liftup.UseVisualStyleBackColor = true;
+            this.liftup.Click += new System.EventHandler(this.liftup_Click);
+            // 
+            // dropdown
+            // 
+            this.dropdown.Location = new System.Drawing.Point(7, 285);
+            this.dropdown.Name = "dropdown";
+            this.dropdown.Size = new System.Drawing.Size(115, 23);
+            this.dropdown.TabIndex = 6;
+            this.dropdown.Text = "dropdown";
+            this.dropdown.UseVisualStyleBackColor = true;
+            this.dropdown.Click += new System.EventHandler(this.dropdown_Click);
+            // 
             // panel2
             // 
             this.panel2.Controls.Add(this.button3);
@@ -180,7 +249,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(99, 80);
+            this.button3.Location = new System.Drawing.Point(129, 80);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(100, 28);
             this.button3.TabIndex = 4;
@@ -193,29 +262,29 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(22, 20);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(47, 15);
+            this.label4.Size = new System.Drawing.Size(87, 15);
             this.label4.TabIndex = 2;
-            this.label4.Text = "theta";
+            this.label4.Text = "theta(deg)";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(22, 52);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(71, 15);
+            this.label5.Size = new System.Drawing.Size(103, 15);
             this.label5.TabIndex = 3;
-            this.label5.Text = "distance";
+            this.label5.Text = "distance(mm)";
             // 
             // target_theta
             // 
-            this.target_theta.Location = new System.Drawing.Point(99, 17);
+            this.target_theta.Location = new System.Drawing.Point(129, 18);
             this.target_theta.Name = "target_theta";
             this.target_theta.Size = new System.Drawing.Size(100, 25);
             this.target_theta.TabIndex = 0;
             // 
             // target_distance
             // 
-            this.target_distance.Location = new System.Drawing.Point(99, 49);
+            this.target_distance.Location = new System.Drawing.Point(129, 49);
             this.target_distance.Name = "target_distance";
             this.target_distance.Size = new System.Drawing.Size(100, 25);
             this.target_distance.TabIndex = 1;
@@ -234,7 +303,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(99, 80);
+            this.button2.Location = new System.Drawing.Point(129, 80);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 4;
@@ -247,22 +316,22 @@
             this.target_X.AutoSize = true;
             this.target_X.Location = new System.Drawing.Point(22, 20);
             this.target_X.Name = "target_X";
-            this.target_X.Size = new System.Drawing.Size(15, 15);
+            this.target_X.Size = new System.Drawing.Size(47, 15);
             this.target_X.TabIndex = 2;
-            this.target_X.Text = "x";
+            this.target_X.Text = "x(mm)";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(22, 52);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(15, 15);
+            this.label3.Size = new System.Drawing.Size(47, 15);
             this.label3.TabIndex = 3;
-            this.label3.Text = "y";
+            this.label3.Text = "y(mm)";
             // 
             // targetX
             // 
-            this.targetX.Location = new System.Drawing.Point(99, 17);
+            this.targetX.Location = new System.Drawing.Point(129, 17);
             this.targetX.Name = "targetX";
             this.targetX.Size = new System.Drawing.Size(100, 25);
             this.targetX.TabIndex = 0;
@@ -270,72 +339,44 @@
             // 
             // targetY
             // 
-            this.targetY.Location = new System.Drawing.Point(99, 49);
+            this.targetY.Location = new System.Drawing.Point(129, 49);
             this.targetY.Name = "targetY";
             this.targetY.Size = new System.Drawing.Size(100, 25);
             this.targetY.TabIndex = 1;
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.pictureBox1);
             this.groupBox3.Controls.Add(this.textBox1);
             this.groupBox3.Location = new System.Drawing.Point(524, 24);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(240, 364);
+            this.groupBox3.Size = new System.Drawing.Size(408, 364);
             this.groupBox3.TabIndex = 10;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Logging";
             // 
-            // dropdown
+            // pictureBox1
             // 
-            this.dropdown.Location = new System.Drawing.Point(7, 285);
-            this.dropdown.Name = "dropdown";
-            this.dropdown.Size = new System.Drawing.Size(115, 23);
-            this.dropdown.TabIndex = 6;
-            this.dropdown.Text = "dropdown";
-            this.dropdown.UseVisualStyleBackColor = true;
-            this.dropdown.Click += new System.EventHandler(this.dropdown_Click);
-            // 
-            // liftup
-            // 
-            this.liftup.Location = new System.Drawing.Point(7, 314);
-            this.liftup.Name = "liftup";
-            this.liftup.Size = new System.Drawing.Size(115, 23);
-            this.liftup.TabIndex = 7;
-            this.liftup.Text = "liftup";
-            this.liftup.UseVisualStyleBackColor = true;
-            this.liftup.Click += new System.EventHandler(this.liftup_Click);
-            // 
-            // activateSucker
-            // 
-            this.activateSucker.Location = new System.Drawing.Point(135, 285);
-            this.activateSucker.Name = "activateSucker";
-            this.activateSucker.Size = new System.Drawing.Size(115, 23);
-            this.activateSucker.TabIndex = 8;
-            this.activateSucker.Text = "Activate";
-            this.activateSucker.UseVisualStyleBackColor = true;
-            this.activateSucker.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // deactivateSucker
-            // 
-            this.deactivateSucker.Location = new System.Drawing.Point(135, 314);
-            this.deactivateSucker.Name = "deactivateSucker";
-            this.deactivateSucker.Size = new System.Drawing.Size(115, 23);
-            this.deactivateSucker.TabIndex = 9;
-            this.deactivateSucker.Text = "Deactivate";
-            this.deactivateSucker.UseVisualStyleBackColor = true;
-            this.deactivateSucker.Click += new System.EventHandler(this.deactivateSucker_Click);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 135);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(407, 229);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(793, 435);
+            this.ClientSize = new System.Drawing.Size(970, 428);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox3);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "cmd pannel";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
@@ -345,6 +386,7 @@
             this.panel1.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -379,6 +421,9 @@
         private System.Windows.Forms.Button activateSucker;
         private System.Windows.Forms.Button liftup;
         private System.Windows.Forms.Button dropdown;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton_wl;
+        private System.Windows.Forms.RadioButton radioButton_wc;
     }
 }
 
--- arm_machine/Form1.cs
+++ arm_machine/Form1.cs
@@ -13,7 +13,7 @@ namespace arm_machine
 {
     public partial class Form1 : Form
     {
-       public SerialPort coom = new SerialPort();
+        public SerialPort coom = new SerialPort();
 
         private Byte motionMode = 0x00;
         private double target_x;
@@ -21,34 +21,70 @@ namespace arm_machine
         private double target_t;
         private double target_d;
 
+        // canvas and coordinate
+        private UInt16 height = 1000;
+        private UInt16 width = 1500;
+
+        private Pen mypen_r = new Pen(Color.Red);
+        private Pen mypen_b = new Pen(Color.Black);
+
+        private Graphics g = null;
+
+        static int center_x, center_y;
+
+
+
+        // config for the lora module
+        // cmd array will be like this:
+        // addr_h, addr_l, channel, data[0], data[1], ......
+        private const Byte lora_target_addr_h = 0;
+        private const Byte lora_target_addr_l = 1;
+        private const Byte lora_target_channel = 15;
+
+        // serial data for lora transport--wireless
+        private Byte[] loraDataCmd = new Byte[53];
+
+        // serial data for TTL serial transport--with cable connect to the machine
         public Byte[] dataCmd = new Byte[50];
 
         public Form1()
         {
             InitializeComponent();
+
+            serial_check(); // refresh the avalible ports
+            this.baudrate.Text = "115200"; // baudrate
+            this.targetX.Text = "100"; // mm
+            this.targetY.Text = "100"; // mm
+
+            this.target_distance.Text = "100"; // mm
+            this.target_theta.Text = "0"; // deg
+
+            
+
         }
 
         private void serial_check()
         {
             // 检查是否有串口
+            // check the serial port
             string[] str = SerialPort.GetPortNames();
 
             if (str == null)
             {
-                MessageBox.Show("本机没有串口！", "Error");
+                MessageBox.Show("本机没有串口！\r No serial port detected", "Error");
                 return;
             }
 
-            //添加串口项目
+            // 添加串口项目 List all the avalible ports
             foreach (string s in System.IO.Ports.SerialPort.GetPortNames())
-            {//获取有多少个COM口
+            {// 获取有多少个COM口
                 if (!serialport.Items.Contains(s))
                 {
                     serialport.Items.Add(s);
                 }
             }
 
-            //串口设置默认选择项
+            //串口设置默认选择项 set the default port
             serialport.SelectedIndex = 0;         //设置cbSerial的默认选项
         }
 
@@ -57,7 +93,7 @@ namespace arm_machine
             if (coom.IsOpen)
             {
                 coom.Close();
-//                disable_buttons();
+                // disable_buttons();
             }
             else
             {
@@ -67,11 +103,12 @@ namespace arm_machine
                 coom.DataBits = 8;
                 coom.StopBits = StopBits.One;
                 Console.WriteLine(coom.BaudRate);
+
                 try
                 {
                     coom.Open();
-                    //                    MessageBox.Show("连接成功");
- //                   enable_buttons();
+//                  MessageBox.Show("连接成功");
+//                  enable_buttons();
                 }
                 catch (Exception ex)
                 {
@@ -100,32 +137,40 @@ namespace arm_machine
         {
             double target_X = double.Parse(this.targetX.Text);
             double target_Y = double.Parse(this.targetY.Text);
-            motionMode = 0x00;
-            target_x = target_X;
-            target_y = target_Y;
 
-            dataCmd[0] = 0x55;
-            dataCmd[1] = 0xaa;
-            dataCmd[2] = (Byte)motionMode;
-            if(dataCmd[2] == 0x00)
+            if(check_coordinate(target_X, target_Y, 1))
             {
-                Int32 x = (Int32)(target_x * 1000);
-                Int32 y = (Int32)(target_y *1000);
-                dataCmd[3] = (Byte)(x >> 24 & 0xff);
-                dataCmd[4] = (Byte)(x >> 16 & 0xff);
-                dataCmd[5] = (Byte)(x >> 8 & 0xff);
-                dataCmd[6] = (Byte)(x >> 0 & 0xff);
-                dataCmd[7] = (Byte)(y >> 24& 0xff);
-                dataCmd[8] = (Byte)(y >> 16 & 0xff);
-                dataCmd[9] = (Byte)(y >> 8 & 0xff);
-                dataCmd[10] = (Byte)(y >> 0 & 0xff);
-            }
-            dataCmd[11] = 0x00;
-            dataCmd[12] = 0x00;
-            dataCmd[13] = 0x0d;
-            dataCmd[14] = 0x0a;
-            coom.Write(dataCmd, 0, 15);
+                motionMode = 0x00;
+                target_x = target_X;
+                target_y = target_Y;
+                dataCmd[0] = 0x55;
+                dataCmd[1] = 0xaa;
+                dataCmd[2] = (Byte)motionMode;
+                if (dataCmd[2] == 0x00)
+                {
+                    UInt32 x = (UInt32)(target_x * 1000); // 单位:mm * 1000 ==> um
+                    UInt32 y = (UInt32)(target_y * 1000);
+                    dataCmd[3] = (Byte)(x >> 24 & 0xff);
+                    dataCmd[4] = (Byte)(x >> 16 & 0xff);
+                    dataCmd[5] = (Byte)(x >> 8 & 0xff);
+                    dataCmd[6] = (Byte)(x >> 0 & 0xff);
+                    dataCmd[7] = (Byte)(y >> 24 & 0xff);
+                    dataCmd[8] = (Byte)(y >> 16 & 0xff);
+                    dataCmd[9] = (Byte)(y >> 8 & 0xff);
+                    dataCmd[10] = (Byte)(y >> 0 & 0xff);
+                }
+                dataCmd[11] = 0x00;
+                dataCmd[12] = 0x00;
+                dataCmd[13] = 0x0d;
+                dataCmd[14] = 0x0a;
 
+                sendDataToMachine(dataCmd, 15);
+
+            }
+            else
+            {
+                MessageBox.Show("Not in the range, please check!", "Error");
+            }
         }
 
         private void targetX_TextChanged(object sender, EventArgs e)
@@ -137,31 +182,41 @@ namespace arm_machine
         {
             double target_Theta = double.Parse(this.target_theta.Text);
             double target_Distance = double.Parse(this.target_distance.Text);
-            motionMode = 0x01;
-            target_t = target_Theta;
-            target_d = target_Distance;
 
-            dataCmd[0] = 0x55;
-            dataCmd[1] = 0xaa;
-            dataCmd[2] = (Byte)motionMode;
-            if (dataCmd[2] == 0x01)
+            if (check_coordinate(target_Theta, target_Distance, 2))
             {
-                Int32 theta = (Int32)(target_t * 1000);
-                Int32 distance = (Int32)(target_d * 1000);
-                dataCmd[3] = (Byte)(theta >> 24& 0xff);
-                dataCmd[4] = (Byte)(theta >> 16 & 0xff);
-                dataCmd[5] = (Byte)(theta >> 8 & 0xff);
-                dataCmd[6] = (Byte)(theta >> 0 & 0xff);
-                dataCmd[7] = (Byte)(distance >> 24 & 0xff);
-                dataCmd[8] = (Byte)(distance >> 16 & 0xff);
-                dataCmd[9] = (Byte)(distance >> 8 & 0xff);
-                dataCmd[10] = (Byte)(distance >> 0 & 0xff);
+
+                motionMode = 0x01;
+                target_t = target_Theta;
+                target_d = target_Distance;
+
+                dataCmd[0] = 0x55;
+                dataCmd[1] = 0xaa;
+                dataCmd[2] = (Byte)motionMode;
+                if (dataCmd[2] == 0x01)
+                {
+                    UInt32 theta = (UInt32)(target_t * 1000);
+                    UInt32 distance = (UInt32)(target_d * 1000);
+                    dataCmd[3] = (Byte)(theta >> 24& 0xff);
+                    dataCmd[4] = (Byte)(theta >> 16 & 0xff);
+                    dataCmd[5] = (Byte)(theta >> 8 & 0xff);
+                    dataCmd[6] = (Byte)(theta >> 0 & 0xff);
+                    dataCmd[7] = (Byte)(distance >> 24 & 0xff);
+                    dataCmd[8] = (Byte)(distance >> 16 & 0xff);
+                    dataCmd[9] = (Byte)(distance >> 8 & 0xff);
+                    dataCmd[10] = (Byte)(distance >> 0 & 0xff);
+                }
+                dataCmd[11] = 0x00;
+                dataCmd[12] = 0x00;
+                dataCmd[13] = 0x0d;
+                dataCmd[14] = 0x0a;
+                sendDataToMachine(dataCmd, 15);
             }
-            dataCmd[11] = 0x00;
-            dataCmd[12] = 0x00;
-            dataCmd[13] = 0x0d;
-            dataCmd[14] = 0x0a;
-            coom.Write(dataCmd, 0, 15);
+            else
+            {
+                MessageBox.Show("Not in the range, please check!", "Error");
+            }
+
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -174,7 +229,7 @@ namespace arm_machine
             dataCmd[5] = 0x00;
             dataCmd[6] = 0x0d;
             dataCmd[7] = 0x0a;
-            coom.Write(dataCmd, 0, 8);
+            sendDataToMachine(dataCmd, 8);
         }
 
         private void dropdown_Click(object sender, EventArgs e)
@@ -187,8 +242,7 @@ namespace arm_machine
             dataCmd[5] = 0x00;
             dataCmd[6] = 0x0d;
             dataCmd[7] = 0x0a;
-            coom.Write(dataCmd, 0, 8);
-
+            sendDataToMachine(dataCmd, 8);
         }
 
         private void liftup_Click(object sender, EventArgs e)
@@ -201,7 +255,7 @@ namespace arm_machine
             dataCmd[5] = 0x00;
             dataCmd[6] = 0x0d;
             dataCmd[7] = 0x0a;
-            coom.Write(dataCmd, 0, 8);
+            sendDataToMachine(dataCmd, 8);
 
         }
 
@@ -215,7 +269,61 @@ namespace arm_machine
             dataCmd[5] = 0x00;
             dataCmd[6] = 0x0d;
             dataCmd[7] = 0x0a;
-            coom.Write(dataCmd, 0, 8);
+            sendDataToMachine(dataCmd, 8);
+
+        }
+
+
+        private bool check_coordinate(double a, double b, Int16 type)
+        {
+            double x, y, theta = 0, distance = 0;
+            double theta_thre_max = 180.0; // deg
+            double theta_thre_min = 0.0;
+            double distance_thre_max = 300; // mm
+            double distance_thre_min = 100;
+
+            switch (type)
+            {
+                case 1: // (x, y)
+                    x = a;
+                    y = b;
+                    distance = Math.Sqrt(x * x + y * y); // mm
+                    theta = Math.Atan2(y, x) * 180 / Math.PI; // deg
+                    break;
+                case 2: // (theta, distance)
+                    theta = a;
+                    distance = b;
+                    break;
+            }
+
+            if(theta <= theta_thre_max && theta >= theta_thre_min && distance <= distance_thre_max && distance >= distance_thre_min)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void sendDataToMachine(Byte[] data, UInt16 len)
+        {
+            if (radioButton_wc.Checked)
+            {
+                coom.Write(dataCmd, 0, len);
+                Console.WriteLine("with cable\n");
+            }
+            else if (radioButton_wl.Checked)
+            {
+                loraDataCmd[0] = lora_target_addr_h;
+                loraDataCmd[1] = lora_target_addr_l;
+                loraDataCmd[2] = lora_target_channel;
+                dataCmd.CopyTo(loraDataCmd, 3);
+                coom.Write(loraDataCmd, 0, len + 3);
+                Console.WriteLine("wireless\n");
+            }
+        }
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
 
         }
     }
